253883799db6a6e63840d2f48952f6cdad86edaf
komoto415 <jeffreyng99@gmail.com>
2019-09-15 02:02:47 -0500

    Commented and fixed variable name of function 4

--- InClassPHPPractice/InClass2.php
+++ InClassPHPPractice/InClass2.php
@@ -12,6 +12,10 @@
 
     <?php
     // Function #1
+    // Take in two strings and compare
+    // if string1 longer: right align and red text
+    // if string2 longer: center align and blue test
+    // if even length: left align and black text
     $string1 = "Jeffrey"; $string2 = "Bob";
     function compareStringsToFormat($string1, $string2) {
       if (strlen($string1) > strlen($string2)) {
@@ -29,6 +33,8 @@
     compareStringsToFormat($string2, $string1);
 
     // Function #2
+    // Find biggest int out of 3 ints
+    // Which ever int is the largest, set its font size to 42
     $x = 14; $y = 12; $z = 10;
     function whichIstheBiggest($int1, $int2, $int3) {
       $greatestNum = max(array($int1, $int2, $int3));
@@ -51,6 +57,7 @@
 
 
     // Function #3
+    // Take a string of an image reference and create and image tag to display on page
     $image1 = "https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Eminem_Killshot.jpg/220px-Eminem_Killshot.jpg";
     $image2 = "https://images.genius.com/4a6058e1375a29d250945d7c9d47ee9d.1000x1000x1.jpg";
     function takeImageFileNameAndTagIt($string1) {
@@ -61,18 +68,21 @@
 
 
     // Function 4
+    // Takes two ints, first one being the array size, the second the upperbound of a random num gen
+    // Populate the array with the random int
+    // Generate and output the average of array
     $test1_size = 10; $test1_integer = 50;
     $test2_size = ; $test2_integer = ;
     function generateFunArray($arraySize, $integerUpperBound) {
       $funArray = array();
-      $arrayAverageDomi = 0;
+      $arraySum = 0;
       $arrayAverage = 0;
       for ($i = 0; $i < $arraySizel; $i++) {
         $insertMe = rand(0,$integerUpperBound);
         $my_array[$i] = $insertMe;
-        $arrayAverageDomi += $insertMe;
+        $arraySum += $insertMe;
       }
-      $arrayAverage = $arrayAverageDomi / $arraySize;
+      $arrayAverage = $arraySum / $arraySize;
       echo "<p style=\"text-alight:center\"> $arrayAverage</p>";
     }
 
